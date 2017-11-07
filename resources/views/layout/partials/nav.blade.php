524ab92e816d5c234bd1b7a4d0978adedf78aa8d
Shareful Islam <km.shareful@gmail.com>
2017-11-08 00:54:37 +0600

    automated menu active feature added

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -14,8 +14,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="{{ route('clients.create') }}">New Client <span class="sr-only">(current)</span></a></li>
-        <li><a href="{{ route('clients.index') }}">List</a></li>
+        <li class="{{ set_active(['clients/create']) }}"><a href="{{ route('clients.create') }}">New Client <span class="sr-only">(current)</span></a></li>
+        <li class="{{ set_active(['clients']) }}"><a href="{{ route('clients.index') }}">List</a></li>
       </ul>      
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
